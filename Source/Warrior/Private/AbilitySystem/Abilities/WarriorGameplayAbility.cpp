d871f476534782e6b9406c9ccfd00152c9815d3c
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-03-06 18:34:22 +0800

    Activation Policy done

---
+++ Source/Warrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
@@ -0,0 +1,31 @@
+// Vince Petrelli All Rights Reserved
+
+
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+
+void UWarriorGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	if (AbilityActivationPolicy == EWarriorAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UWarriorGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	if (AbilityActivationPolicy == EWarriorAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+}
---
+++ Source/Warrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -0,0 +1,32 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "WarriorGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class EWarriorAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	//~ Begin UGameplayAbility Interface.
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec);
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+	//~ End UGameplayAbility Interface
+
+	UPROPERTY(EditDefaultsOnly, Category = "WarriorAbility")
+	EWarriorAbilityActivationPolicy AbilityActivationPolicy = EWarriorAbilityActivationPolicy::OnTriggered;
+
+};
