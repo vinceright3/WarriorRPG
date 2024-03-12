a71660e004d0d3856283055bfffc7be43ac347da
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-03-12 17:52:28 +0800

    Wait for gameplay event done

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -13,4 +13,7 @@ namespace WarriorGameplayTags
 
 	/** Player tags **/
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe,"Player.Weapon.Axe");
+
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe,"Player.Event.Equip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Unequip_Axe,"Player.Event.Unequip.Axe");
 }
--- Source/Warrior/Public/WarriorGameplayTags.h
+++ Source/Warrior/Public/WarriorGameplayTags.h
@@ -14,4 +14,7 @@ namespace WarriorGameplayTags
 
 	/** Player tags **/
 	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Weapon_Axe);
+
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Equip_Axe);
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Unequip_Axe);
 }
