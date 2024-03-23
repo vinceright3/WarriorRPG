94fbaff4a4a2ef8a11e2df76e27b720a90408838
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-03-23 14:37:43 +0800

    Attribute set done

--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -3,3 +3,12 @@
 
 #include "AbilitySystem/WarriorAttributeSet.h"
 
+UWarriorAttributeSet::UWarriorAttributeSet()
+{
+	InitCurrentHealth(1.f);
+	InitMaxHealth(1.f);
+	InitCurrentRage(1.f);
+	InitMaxRage(1.f);
+	InitAttackPower(1.f);
+	InitDefensePower(1.f);
+}
--- Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
+++ Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "WarriorAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +21,30 @@ class WARRIOR_API UWarriorAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UWarriorAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,CurrentRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,MaxRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,AttackPower)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,DefensePower)
 };
