3657cba4b50905ff15a7e9f918480b904cbf35cc
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-03-30 14:51:33 +0800

    Retrieve damage info done

--- Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -43,3 +43,44 @@ UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
 }
 
+void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{	
+	const FGameplayEffectSpec& EffectSpec = ExecutionParams.GetOwningSpec();
+	
+	/*EffectSpec.GetContext().GetSourceObject();
+	EffectSpec.GetContext().GetAbility();
+	EffectSpec.GetContext().GetInstigator();
+	EffectSpec.GetContext().GetEffectCauser();*/
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = EffectSpec.CapturedSourceTags.GetAggregatedTags();
+	EvaluateParameters.TargetTags = EffectSpec.CapturedTargetTags.GetAggregatedTags();
+
+	float SourceAttackPower = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().AttackPowerDef,EvaluateParameters,SourceAttackPower);
+
+	float BaseDamage = 0.f;
+	int32 UsedLightAttckComboCount = 0;
+	int32 UsedHeavyAttackComboCount = 0;
+
+	for (const TPair<FGameplayTag, float>& TagMagnitude : EffectSpec.SetByCallerTagMagnitudes)
+	{
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Shared_SetByCaller_BaseDamage))
+		{
+			BaseDamage = TagMagnitude.Value;
+		}
+
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Light))
+		{
+			UsedLightAttckComboCount = TagMagnitude.Value;
+		}
+
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Heavy))
+		{
+			UsedHeavyAttackComboCount = TagMagnitude.Value;
+		}
+	}
+
+	float TargetDefensePower = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().DefensePowerDef,EvaluateParameters,TargetDefensePower);
+}
--- Source/Warrior/Public/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h
+++ Source/Warrior/Public/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h
@@ -17,4 +17,5 @@ class WARRIOR_API UGEExecCalc_DamageTaken : public UGameplayEffectExecutionCalcu
 public:
 	UGEExecCalc_DamageTaken();
 
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
 };
