8f016db4c97002a73fde522b43ad1059c75e1ec7
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-04-29 14:47:52 +0800

    NewNativeServiceDone

---
+++ Source/Warrior/Private/AI/BTService_OrientToTargetActor.cpp
@@ -0,0 +1,55 @@
+// Vince Petrelli All Rights Reserved
+
+
+#include "AI/BTService_OrientToTargetActor.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UBTService_OrientToTargetActor::UBTService_OrientToTargetActor()
+{
+	NodeName = TEXT("Native Orient Rotation To Target Actor");
+
+	INIT_SERVICE_NODE_NOTIFY_FLAGS();
+
+	RotationInterpSpeed = 5.f;
+	Interval = 0.f;
+	RandomDeviation = 0.f;
+
+	InTargetActorKey.AddObjectFilter(this,GET_MEMBER_NAME_CHECKED(ThisClass,InTargetActorKey),AActor::StaticClass());
+}
+
+void UBTService_OrientToTargetActor::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	if (UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		InTargetActorKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+FString UBTService_OrientToTargetActor::GetStaticDescription() const
+{	
+	const FString KeyDescription = InTargetActorKey.SelectedKeyName.ToString();
+
+	return FString::Printf(TEXT("Orient rotation to %s Key %s"),*KeyDescription,*GetStaticServiceDescription());
+}
+
+void UBTService_OrientToTargetActor::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UObject* ActorObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(InTargetActorKey.SelectedKeyName);
+	AActor* TargetActor = Cast<AActor>(ActorObject);
+
+	APawn* OwningPawn = OwnerComp.GetAIOwner()->GetPawn();
+
+	if (OwningPawn && TargetActor)
+	{
+		const FRotator LookAtRot = UKismetMathLibrary::FindLookAtRotation(OwningPawn->GetActorLocation(),TargetActor->GetActorLocation());
+		const FRotator TargetRot = FMath::RInterpTo(OwningPawn->GetActorRotation(),LookAtRot,DeltaSeconds,RotationInterpSpeed);
+
+		OwningPawn->SetActorRotation(TargetRot);
+	}
+}
---
+++ Source/Warrior/Public/AI/BTService_OrientToTargetActor.h
@@ -0,0 +1,31 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_OrientToTargetActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UBTService_OrientToTargetActor : public UBTService
+{
+	GENERATED_BODY()
+	
+	UBTService_OrientToTargetActor();
+
+	//~ Begin UBTNode Interface
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual FString GetStaticDescription() const override;
+	//~ End UBTNode Interface
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Target")
+	FBlackboardKeySelector InTargetActorKey;
+
+	UPROPERTY(EditAnywhere, Category = "Target")
+	float RotationInterpSpeed;
+};
