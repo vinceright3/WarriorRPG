26ae996d5a93796d19b235205684384a0122900c
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-05-06 15:01:43 +0800

    Construct Native Task Done

---
+++ Source/Warrior/Private/AI/BTTask_RotateToFaceTarget.cpp
@@ -0,0 +1,41 @@
+// Vince Petrelli All Rights Reserved
+
+
+#include "AI/BTTask_RotateToFaceTarget.h"
+
+UBTTask_RotateToFaceTarget::UBTTask_RotateToFaceTarget()
+{
+	NodeName = TEXT("Native Rotate to Face Target Actor");
+	AnglePrecision = 10.f;
+	RotationInterpSpeed = 5.f;
+
+	bNotifyTick = true;
+	bNotifyTaskFinished = true;
+	bCreateNodeInstance = false;
+
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+
+	InTargetToFaceKey.AddObjectFilter(this,GET_MEMBER_NAME_CHECKED(ThisClass,InTargetToFaceKey),AActor::StaticClass());
+}
+
+void UBTTask_RotateToFaceTarget::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	if (UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		InTargetToFaceKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+uint16 UBTTask_RotateToFaceTarget::GetInstanceMemorySize() const
+{
+	return sizeof(FRotateToFaceTargetTaskMemory);
+}
+
+FString UBTTask_RotateToFaceTarget::GetStaticDescription() const
+{	
+	const FString KeyDescription = InTargetToFaceKey.SelectedKeyName.ToString();
+
+	return FString::Printf(TEXT("Smoothly rotates to face %s Key until the angle precision: %s is reached"),*KeyDescription,*FString::SanitizeFloat(AnglePrecision));
+}
---
+++ Source/Warrior/Public/AI/BTTask_RotateToFaceTarget.h
@@ -0,0 +1,50 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_RotateToFaceTarget.generated.h"
+
+struct FRotateToFaceTargetTaskMemory
+{
+	TWeakObjectPtr<APawn> OwningPawn;
+	TWeakObjectPtr<AActor> TargetActor;
+
+	bool IsValid() const
+	{
+		return OwningPawn.IsValid() && TargetActor.IsValid();
+	}
+
+	void Reset()
+	{
+		OwningPawn.Reset();
+		TargetActor.Reset();
+	}
+};
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UBTTask_RotateToFaceTarget : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	UBTTask_RotateToFaceTarget();
+	
+	//~ Begin UBTNode Interface
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual uint16 GetInstanceMemorySize() const override;
+	virtual FString GetStaticDescription() const override;
+	//~ End UBTNode Interface
+
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	float AnglePrecision;
+
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	float RotationInterpSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Face Target")
+	FBlackboardKeySelector InTargetToFaceKey;
+};
