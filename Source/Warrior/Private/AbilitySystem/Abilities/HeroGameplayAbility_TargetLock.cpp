89c0ffd29da54be6fc9b793a8e5447e862d60a0e
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-05-25 13:08:47 +0800

    GetAvailableTargetsDone

--- Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -2,13 +2,56 @@
 
 
 #include "AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Characters/WarriorHeroCharacter.h"
+
+#include "WarriorDebugHelper.h"
 
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {	
+	TryLockOnTarget();
+
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
 void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
+{
+	GetAvailableActorsToLock();
+}
+
+void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{	
+	TArray<FHitResult> BoxTraceHits;
+
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetHeroCharacterFromActorInfo(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation(),
+		GetHeroCharacterFromActorInfo()->GetActorLocation() + GetHeroCharacterFromActorInfo()->GetActorForwardVector() * BoxTraceDistance,
+		TraceBoxSize / 2.f,
+		GetHeroCharacterFromActorInfo()->GetActorForwardVector().ToOrientationRotator(),
+		BoxTraceChannel,
+		false,
+		TArray<AActor*>(),
+		bShowPersistentDebugShape? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,
+		BoxTraceHits,
+		true
+	);
+
+	for (const FHitResult& TraceHit : BoxTraceHits)
+	{
+		if (AActor* HitActor = TraceHit.GetActor())
+		{
+			if (HitActor != GetHeroCharacterFromActorInfo())
+			{
+				AvailableActorsToLock.AddUnique(HitActor);
+
+				Debug::Print(HitActor->GetActorNameOrLabel());
+			}
+		}
+	}
 }
--- Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -19,4 +19,23 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	//~ End UGameplayAbility Interface
+
+private:
+	void TryLockOnTarget();
+	void GetAvailableActorsToLock();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	float BoxTraceDistance = 5000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	FVector TraceBoxSize = FVector(5000.f,5000.f,300.f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	TArray< TEnumAsByte < EObjectTypeQuery > > BoxTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	bool bShowPersistentDebugShape = false;
+
+	UPROPERTY()
+	TArray<AActor*> AvailableActorsToLock;
 };
