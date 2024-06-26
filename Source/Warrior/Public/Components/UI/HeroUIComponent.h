2a202177283ffc5eea256a72113aaa0eb9133db3
vinceright3 <96793666+vinceright3@users.noreply.github.com>
2024-06-26 16:09:21 +0800

    NotifyAbilityCooldownDone

--- Source/Warrior/Public/Components/UI/HeroUIComponent.h
+++ Source/Warrior/Public/Components/UI/HeroUIComponent.h
@@ -11,6 +11,8 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate,TSo
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAbilityIconSlotUpdatedDelegate,FGameplayTag,AbilityInputTag,TSoftObjectPtr<UMaterialInterface>,SoftAbilityIconMaterial);
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnAbilityCooldownBeginDelegate,FGameplayTag,AbilityInputTag,float,TotalCooldownTime,float,RemainingCooldownTime);
+
 /**
  * 
  */
@@ -28,4 +30,7 @@ public:
 
 	UPROPERTY(BlueprintCallable,BlueprintAssignable)
 	FOnAbilityIconSlotUpdatedDelegate OnAbilityIconSlotUpdated;
+
+	UPROPERTY(BlueprintCallable,BlueprintAssignable)
+	FOnAbilityCooldownBeginDelegate OnAbilityCooldownBegin;
 };
